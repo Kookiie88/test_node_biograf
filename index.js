903d9915f4b68d0f43362bfcb3efee9223574248
Kookiie88 <alexander@jj-media.se>
2024-01-20 22:27:52 +0700

    fixed some code

--- index.js
+++ index.js
@@ -10,42 +10,28 @@ app.get("/", async (request, response) => {
   response.send(html);
 });
 
-app.get("/index.html", async (request, response) => {
-  const buf = await fs.readFile("./index.html");
-  const html = buf.toString();
-
-  response.send(html);
-});
-
-app.get("/aboutus.html", async (request, response) => {
+app.get("/aboutus", async (request, response) => {
   const buf = await fs.readFile("./aboutus.html");
   const html = buf.toString();
 
   response.send(html);
 });
 
-app.get("/filmer.html", async (request, response) => {
+app.get("/filmer", async (request, response) => {
   const buf = await fs.readFile("./filmer.html");
   const html = buf.toString();
 
   response.send(html);
 });
 
-app.get("/filmer.html", async (request, response) => {
-  const buf = await fs.readFile("./filmer.html");
-  const html = buf.toString();
-
-  response.send(html);
-});
-
-app.get("/newsevents.html", async (request, response) => {
+app.get("/newsevents", async (request, response) => {
   const buf = await fs.readFile("./newsevents.html");
   const html = buf.toString();
 
   response.send(html);
 });
 
-app.get("/oursponsors.html", async (request, response) => {
+app.get("/oursponsors", async (request, response) => {
   const buf = await fs.readFile("./oursponsors.html");
   const html = buf.toString();
 
