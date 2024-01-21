777b16899c2c326d39713913e790ee0e91c908fb
Kookiie88 <alexander@jj-media.se>
2024-01-22 04:52:18 +0700

    made everything work with handlebar execept menu

--- index.js
+++ index.js
@@ -1,41 +1,42 @@
 import express from "express";
+import { engine } from "express-handlebars";
 import fs from "fs/promises";
 
 const app = express();
+app.engine("handlebars", engine());
+app.set("view engine", "handlebars");
+app.set("views", "./templates");
+
+const menu = [
+  { name: "index", url: "/" },
+  { name: "aboutus", url: "/aboutus" },
+  { name: "fi lmer", url: "/filmer" },
+  { name: "newsevents", url: "/newsevents" },
+  { name: "oursponsors", url: "/oursponsors" },
+];
+
+async function renderPage(response, page) {
+  response.render(page);
+}
 
 app.get("/", async (request, response) => {
-  const buf = await fs.readFile("./index.html");
-  const html = buf.toString();
-
-  response.send(html);
+  renderPage(response, "index");
 });
 
 app.get("/aboutus", async (request, response) => {
-  const buf = await fs.readFile("./aboutus.html");
-  const html = buf.toString();
-
-  response.send(html);
+  renderPage(response, "aboutus");
 });
 
 app.get("/filmer", async (request, response) => {
-  const buf = await fs.readFile("./filmer.html");
-  const html = buf.toString();
-
-  response.send(html);
+  renderPage(response, "filmer");
 });
 
 app.get("/newsevents", async (request, response) => {
-  const buf = await fs.readFile("./newsevents.html");
-  const html = buf.toString();
-
-  response.send(html);
+  renderPage(response, "newsevents");
 });
 
 app.get("/oursponsors", async (request, response) => {
-  const buf = await fs.readFile("./oursponsors.html");
-  const html = buf.toString();
-
-  response.send(html);
+  renderPage(response, "oursponsors");
 });
 
 app.use("/lib", express.static("./lib"));
